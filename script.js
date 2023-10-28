4434d16b74d00c3086e57b9ffab397769539d053
Harshini Akshaya A S <140588998+harshaya13@users.noreply.github.com>
2023-10-29 01:03:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function signup() {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  
-  // Save user data in localStorage (not secure for production)
+
   localStorage.setItem(email, JSON.stringify({ name, password }));
   
   alert('Signup successful. Please log in.');
@@ -12,8 +11,7 @@ function signup() {
 function login() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  
-  // Retrieve user data from localStorage (not secure for production)
+
   const userData = JSON.parse(localStorage.getItem(email));
   
   if (userData && userData.password === password) {
@@ -25,9 +23,6 @@ function login() {
 
 function forgotPassword() {
   const email = document.getElementById('email').value;
-  
-  // Generate a reset link and send it to the user (not implemented here).
-  // In a real application, you would send an email with a link to reset the password.
-  
+
   alert('A reset link has been sent to your email if it exists in our system.');
 }
